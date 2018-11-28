b3553f0cceb83fde01a74196ee7357702a6684c6
GCoder10 <kacper457g@gmail.com>
2018-11-28 18:51:39 +0100

    commit
    commit

--- src/app/workers/components/worker-detail/worker-detail.component.ts
+++ src/app/workers/components/worker-detail/worker-detail.component.ts
@@ -7,11 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkerDetailComponent implements OnInit {
   bgWorkerDetail: string;
+  chooseInfo: string;
 
   constructor() { }
 
   ngOnInit() {
     this.bgWorkerDetail = 'assets/images/BGsignin2.jpg';
+  }
+
+  onClickCity() {
+    this.chooseInfo = 'city';
+  }
+
+  onClickCountry() {
+    this.chooseInfo = 'country';
+  }
+
+  onClickGender() {
+    this.chooseInfo = 'gender';
+  }
+
+  onClickInterests() {
+    this.chooseInfo = 'interests';
+  }
+
+  onClickIntroduction() {
+    this.chooseInfo = 'introduction';
+  }
+
+  onClickName() {
+    this.chooseInfo = 'name';
+  }
+
+  onClickSurname() {
+    this.chooseInfo = 'surname';
+  }
+
+  onClickDateOfBirth() {
+    this.chooseInfo = 'dateOfBirth';
   }
 
 }
